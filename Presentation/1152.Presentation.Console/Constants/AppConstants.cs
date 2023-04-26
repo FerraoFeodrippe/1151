206390711311728670f2e6e250d43765783ad37b
Ferrao <marcelo@feodrippe.com>
2023-04-26 00:17:59 -0300

    removendo código lixo.

--- Presentation/1152.Presentation.Console/Constants/AppConstants.cs
+++ Presentation/1152.Presentation.Console/Constants/AppConstants.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace _1152.Presentation.Console.Constants
 {
     internal class AppConstants
